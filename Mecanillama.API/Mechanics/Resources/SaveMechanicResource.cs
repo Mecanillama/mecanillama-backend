f3c7cbfdacc1eea5216377d5613042cdc79a0f8d
jrenzoo <u202118568@upc.edu.pe>
2022-06-08 20:36:27 -0500

    feat: added resources for mechanic

---
+++ Mecanillama.API/Mechanics/Resources/SaveMechanicResource.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mecanillama.API.Mechanics.Resources;
+
+public class SaveMechanicResource
+{
+    [Required]
+    [MaxLength(40)]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(40)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(40)]
+    public string Password { get; set; }
+    [Required]
+    [MaxLength(40)]
+    public string Address { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set; }
+    [Required]
+    [MaxLength(10)]
+    public long PhoneNumber { get; set; }
+}
