14b7ff25bf961ed4cef839627b7ca2154650629d
jrenzoo <u202118568@upc.edu.pe>
2022-06-08 20:30:02 -0500

    feat: implemented persistence layer in mechanic

---
+++ Mecanillama.API/Mechanics/Controllers/MechanicsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mecanillama.API.Mechanics.Controllers;
+
+[Route("/api/v1/[controller]")] 
+public class MechanicsController
+{
+    
+}
--- Mecanillama.API/Mechanics/Domain/Models/Mechanic.cs
+++ Mecanillama.API/Mechanics/Domain/Models/Mechanic.cs
@@ -1,18 +1,19 @@
 ﻿using Mecanillama.API.Appointments.Domain.Models;
 
-namespace Mecanillama.API.Customers.Domain.Model;
+namespace Mecanillama.API.Mechanics.Domain.Models;
 
-public class Customer
+public class Mechanic
 {
-    // Properties
+    //Properties
     public long Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string CarMake { get; set; }
     public string Address { get; set; }
-
+    public string Description { get; set; }
+    public string PhoneNumber { get; set; }
+    
     //Relationships
     public IList<Appointment> Appointments { get; set; } = new List<Appointment>();
-
+    //public IList<Review> Reviews {get; set;} = new List<Review>();
 }
