db6009ad4b6f0910e05c8e6a50cafdbc1ce8d68d
Adrian Velasco <lvelascoand@gmail.com>
2022-06-11 15:11:47 -0500

    Revert "feat(reviews): added Reviews bounded context"
    This reverts commit c6e4b9ecee4a45053f778d693d31e38aeb4b4aad.

--- Mecanillama.API/Reviews/Resources/ReviewResource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mecanillama.API.Reviews.Resources;
-
-public class ReviewResource
-{
-    public int Id { get; set; }
-    public string Comment { get; set; }
-    public double Score { get; set; }
-}
