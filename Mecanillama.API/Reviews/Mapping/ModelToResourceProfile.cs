c6e4b9ecee4a45053f778d693d31e38aeb4b4aad
Adrian Velasco <lvelascoand@gmail.com>
2022-06-11 15:00:46 -0500

    feat(reviews): added Reviews bounded context

---
+++ Mecanillama.API/Reviews/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Mecanillama.API.Reviews.Domain.Models;
+using Mecanillama.API.Reviews.Resources;
+
+namespace Mecanillama.API.Reviews.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Review, ReviewResource>();
+    }
+
+}
---
+++ Mecanillama.API/Reviews/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Mecanillama.API.Reviews.Domain.Models;
+using Mecanillama.API.Reviews.Resources;
+
+namespace Mecanillama.API.Reviews.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveReviewResource, Review>();
+    }
+}
