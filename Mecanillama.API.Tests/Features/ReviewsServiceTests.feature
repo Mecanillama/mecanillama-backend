0677cd5f3431fa1d1d297413408373bd661c1022
Adrian Velasco <lvelascoand@gmail.com>
2022-06-11 19:38:47 -0500

    feat: added SpecFlow

---
+++ Mecanillama.API.Tests/Features/ReviewsServiceTests.feature
@@ -0,0 +1,28 @@
+﻿Feature: ReviewsServiceTests
+As a User
+I want to add new Review through API
+In order to make it available for views.
+
+    Background:
+        Given the Endpoint https://localhost:7056/api/v1/reviews is available
+
+    @review-adding
+    Scenario: Add Review with Unique Comment
+        When a Review Request is sent
+          | Comment | Score |
+          | Sample  | 1     |
+        Then A Response with Status 200 is received
+        And a Sample Resource is included in Response Body
+          | Id | Comment | Score |
+          | 1  | Sample  | 1     |
+
+    @review-adding
+    Scenario: Add Review with existing Title
+        Given A Review is already stored
+          | Id | Comment | Score |
+          | 1  | Sample  | 1     |
+        When a Review Request is sent
+          | Comment | Score |
+          | Sample  | 1     |
+        Then A Response with Status 400 is received
+        And An Error Message with value "Review already exists." is returned
---
+++ Mecanillama.API.Tests/Steps/ReviewsServiceStepDefinitions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Mecanillama.API.Tests.Steps;
+
+[Binding]
+public sealed class ReviewsServiceStepDefinitions
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly ScenarioContext _scenarioContext;
+
+    public ReviewsServiceStepDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given("the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [Given("the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        //TODO: implement act (action) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        //TODO: implement assert (verification) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/reviews is available")]
+    public void GivenTheEndpointHttpsLocalhostApiVReviewsIsAvailable(int p0, int p1)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"a Review Request is sent")]
+    public void WhenAReviewRequestIsSent(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"A Response with Status (.*) is received")]
+    public void ThenAResponseWithStatusIsReceived(int p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"a Sample Resource is included in Response Body")]
+    public void ThenASampleResourceIsIncludedInResponseBody(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"A Review is already stored")]
+    public void GivenAReviewIsAlreadyStored(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"An Error Message with value ""(.*)"" is returned")]
+    public void ThenAnErrorMessageWithValueIsReturned(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
