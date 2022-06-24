441a17915fd6a2fc80fda8cbd177f45cbb345965
jrenzoo <u202118568@upc.edu.pe>
2022-06-23 20:42:32 -0500

    feat: implementd repository interface for communication

---
+++ Mecanillama.API/Security/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Mecanillama.API.Security.Domain.Models;
+
+namespace Mecanillama.API.Security.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task AddAsync(User user);
+    Task<User> FindByIdAsync(int id);
+    Task<User> FindByUsernameAsync(string username);
+    bool ExistsByUsername(string username);
+    User FindById(int id);
+    void Update(User user);
+    void Remove(User user);
+}
