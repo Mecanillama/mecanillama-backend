65318c2662e32c2d2851d4ebecd26b29bf020615
jrenzoo <u202118568@upc.edu.pe>
2022-06-23 20:47:02 -0500

    feat: implemented communication files in services layer

---
+++ Mecanillama.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Mecanillama.API.Security.Domain.Services.Communication;
+
+public class AuthenticateResponse
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
---
+++ Mecanillama.API/Security/Domain/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Mecanillama.API.Security.Domain.Models;
+
+namespace Mecanillama.API.Security.Domain.Services;
+
+public interface IUserService
+{
+    Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task RegisterAsync(RegisterRequest request);
+    Task UpdateAsync(int id, UpdateRequest request);
+    Task DeleteAsync(int id);
+}
